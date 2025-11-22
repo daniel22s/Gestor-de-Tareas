d72523cecbacceb69c0a936f50670b5db4191abd
daniel22s <cruzdani543@gmail.com>
2025-11-21 19:55:33 -0500

    Creando Templates

--- task_manage/views.py
+++ task_manage/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    context = {'message':'Bienvenidos al Gestor de Tareas'}
+    return render(request,'index.html',context)
